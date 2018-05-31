e6de9c2ccb8921789659faf50b755c781465172c
arnav dhamija <arnav.dhamija@gmail.com>
2018-05-31 15:51:50 +0530

    Copied code to make rtcp report viewable in gstreamer

--- rtcp_sender.c
+++ rtcp_sender.c
@@ -1,6 +1,7 @@
 #include <gst/gst.h>
-#include <gst/rtp/gstrtcpbuffer.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <gst/rtp/gstrtcpbuffer.h>
 int videoport = 5000;
 int rtcpport = 5005;
 int rtcpsinkport = 5001;
@@ -8,15 +9,59 @@ int bitrate = 1000;
 // char recv_addr[] = "192.168.1.7";
 char recv_addr[] = "127.0.0.1";
 
+static gboolean
+process_rtcp_packet(GstRTCPPacket *packet){
+	guint32 ssrc, rtptime, packet_count, octet_count;
+	guint64 ntptime;
+	guint count, i;
+
+	count = gst_rtcp_packet_get_rb_count(packet);
+	g_debug("    count         %d", count);
+	for (i=0; i<count; i++) {
+		guint32 exthighestseq, jitter, lsr, dlsr;
+		guint8 fractionlost;
+		gint32 packetslost;
+
+		gst_rtcp_packet_get_rb(packet, i, &ssrc, &fractionlost,
+				&packetslost, &exthighestseq, &jitter, &lsr, &dlsr);
+
+		g_warning("    block         %d", i);
+		g_warning("    ssrc          %d", ssrc);
+		g_warning("    highest   seq %d", exthighestseq);
+		g_warning("    jitter        %d", jitter);
+		g_warning("    fraction lost %d", fractionlost);
+		g_warning("    packet   lost %d", packetslost);
+	}
+
+	//g_debug("Received rtcp packet");
+
+	return TRUE;
+}
+
 static void rtcp_recv_callback(GstElement *src, GstBuffer *buf, gpointer data)
 {
-    g_warning("rtcp_received");
-    gboolean res = gst_rtcp_buffer_validate_data(data, 4);
-    if (res == TRUE) {
-        g_warning("Valid buffer");
-    } else {
-        g_warning("Bad buf");
-    }
+    // g_warning("rtcp_received %d", gst_rtcp_buffer_get_packet_count(buf));
+    GstRTCPBuffer *rtcpbuf = malloc(sizeof(GstRTCPBuffer));
+    rtcpbuf->buffer = NULL;
+    // rtcpbuf->map = GST_MAP_INFO_INIT;
+    gst_rtcp_buffer_map(buf, GST_MAP_READ, rtcpbuf);
+    GstRTCPPacket *packet = malloc(sizeof(GstRTCPPacket));
+    gboolean more = gst_rtcp_buffer_get_first_packet(rtcpbuf, packet);
+	while (more) {
+		GstRTCPType type;
+		type = gst_rtcp_packet_get_type(packet);
+		switch (type) {
+		case GST_RTCP_TYPE_RR:
+            process_rtcp_packet(packet);
+			// send_event_to_encoder(venc, &rtcp_pkt);
+			break;
+		default:
+			g_debug("Other types");
+			break;
+		}
+		more = gst_rtcp_packet_move_to_next(packet);
+	}
+
 }
 
 
@@ -34,8 +79,8 @@ int main(int argc, char *argv[])
     GstCaps *caps, *rtcpcaps;
 
     caps = gst_caps_new_simple ("video/x-raw",
-                                "width", G_TYPE_INT, 640,
-                                "height", G_TYPE_INT, 360,
+                                "width", G_TYPE_INT, 1280,
+                                "height", G_TYPE_INT, 720,
                                 "framerate", GST_TYPE_FRACTION, 30, 1,
                                 NULL);
     rtcpcaps = gst_caps_new_simple("application/x-rtcp", NULL); //might not be needed
@@ -51,7 +96,7 @@ int main(int argc, char *argv[])
     rtcpsink = gst_element_factory_make("udpsink", "rtcpsink");
     rtcpsrc = gst_element_factory_make("udpsrc", "rtcpsrc");
     
-    g_object_set(G_OBJECT (source), "device", "/dev/video1", NULL);
+    g_object_set(G_OBJECT (source), "device", "/dev/video0", NULL);
     g_object_set(G_OBJECT (rtcpsrc), "caps", gst_caps_from_string("application/x-rtcp"), "port", rtcpport, NULL);
     g_object_set(G_OBJECT(rtcpsink), "host", recv_addr, "port", rtcpsinkport, NULL);
     g_object_set(G_OBJECT(sink), "host", recv_addr, "port", videoport, NULL);
